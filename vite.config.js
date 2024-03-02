9879a2da0d8ae4c5aa5ab9273061859cdb084260
paoDweb <info@paoDweb.com>
2024-03-02 17:15:16 +0100

    fix vite config

--- vite.config.js
+++ vite.config.js
@@ -13,5 +13,5 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
-  base: "https:// paodweb.github.io/challenge-tri/",
+  base: "/challenge-tri/",
 })
